75adb61b33710bc44a20488fa673a29a6fee5269
Sundararaman <vsundar17697@outlook.com>
2018-05-31 23:50:24 +0530

    Add files via upload

---
+++ optionCreator.js
@@ -0,0 +1,31 @@
+var pieChartOptions = function(actualData , actualTitle = 'Sample Title'  , actualSeriesName = 'Sample Series' , ){
+    return {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: actualTitle
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y:.0f}</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: actualSeriesName,
+                colorByPoint: true,
+                data: actualData
+            }]
+        };
+}
